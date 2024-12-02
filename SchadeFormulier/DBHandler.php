0020d20213621cb8b51394b4b6e352b487bdebec
Teunnemans <91384840+Teuntitaan1@users.noreply.github.com>
2024-12-02 11:17:08 +0100

    Bug fix

--- SchadeFormulier/DBHandler.php
+++ SchadeFormulier/DBHandler.php
@@ -19,7 +19,7 @@ if (isset($_POST['Source'])) {
 
     // Query die de data verwerkt.
     $Query = " INSERT INTO `SchadeServer` (`ToiletID`, `Bron`, `Betrouwbaarheid`, `Beschrijving`, `BewijsNaam`) 
-                VALUES ('".$_POST["ToiletID"]."', '".$_POST["Source"]."', '".$_POST["Validity"]."', '".$FilteredDescription."', '$EvidenceName');";
+                VALUES ('".$_POST["ToiletID"]."', '".$_POST["Source"]."', '".$_POST["Validity"]."', '".$_POST["Description"]."', '$EvidenceName');";
     // SQL injectie poging dan aso wegsturen
     if (substr_count($Query, ";") > 1) { header("location: ./index.php?ToiletID=".$_POST["ToiletID"]."&Done=False"); exit(); }
     else {$Connection->query($Query); $Connection->close();} // Voer de query uit
